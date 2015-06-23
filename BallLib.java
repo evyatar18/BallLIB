fe7d063b74555fbd308dee5e5eb05f2d38c2358e
evyatar18 <evyatar38@gmail.com>
2015-06-23 11:45:26 +0300

    Update BallLib.java

--- BallLib.java
+++ BallLib.java
@@ -20,6 +20,29 @@ public class BallLib {
 	}
 	
 	/**
+	 * Returns a specific location which is in a given distance from a location and lying in specific angles
+	 * @param center - the returned location will be in a distance relative to this location
+	 * @param radius - distance
+	 * @param y_angle - angle among the y-axis and z-axis
+	 * @param xz_angle - angle among the x-axis and z-axis
+	 * @return the location
+	 */
+	public static Location getBallLocation(Location center, double radius, double y_angle, double xz_angle) {
+		y_angle = Math.toRadians(y_angle); // transform angle to radians
+		xz_angle = Math.toRadians(xz_angle); // transform angle to radians
+		
+		double y = Math.sin(y_angle)*radius; // gets the y of the point on the ball at (0, 0, 0)
+		
+		double a = Math.sqrt(Math.pow(radius, 2) - Math.pow(y, 2)); // Pythagoras to get radius of circle of x and z.
+		
+		double x = Math.sin(xz_angle)*a; // gets the x of the point on the ball at (0, 0, 0)
+		double z = Math.cos(xz_angle)*a; // gets the z of the point on the ball at (0, 0, 0)
+		
+		
+		return center.clone().add(x, y, z); // adds the x, y, z found to the location
+	}
+	
+	/**
 	 * Returns locations which will create a ball with given center location, radius and parts
 	 * @param center - center location of the ball
 	 * @param radius - radius of the ball
@@ -53,26 +76,4 @@ public class BallLib {
 		return ball;
 	}
 	
-	/**
-	 * Returns a specific location which is in a given distance from a location and lying in specific angles
-	 * @param center - the returned location will be in a distance relative to this location
-	 * @param radius - distance
-	 * @param y_angle - angle among the y-axis and z-axis
-	 * @param xz_angle - angle among the x-axis and z-axis
-	 * @return the location
-	 */
-	public static Location getBallLocation(Location center, double radius, double y_angle, double xz_angle) {
-		y_angle = Math.toRadians(y_angle); // transform angle to radians
-		xz_angle = Math.toRadians(xz_angle); // transform angle to radians
-		
-		double y = Math.sin(y_angle)*radius; // gets the y of the point on the ball at (0, 0, 0)
-		
-		double a = Math.sqrt(Math.pow(radius, 2) - Math.pow(y, 2)); // Pythagoras to get radius of circle of x and z.
-		
-		double x = Math.sin(xz_angle)*a; // gets the x of the point on the ball at (0, 0, 0)
-		double z = Math.cos(xz_angle)*a; // gets the z of the point on the ball at (0, 0, 0)
-		
-		
-		return center.clone().add(x, y, z); // adds the x, y, z found to the location
-	}
 }
